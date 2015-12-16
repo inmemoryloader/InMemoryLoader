1e1c9a098b733e2a799a7cbe8b3cae222ed017d5
Kay Stuckenschmidt <mailto.kaysta@gmail.com>
2015-12-16 21:37:43 +0100

    Basic Setup and TestConsole with a TestComponent

---
+++ InMemoryLoaderTestComponent/MyClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using InMemoryLoaderBase;
+using log4net;
+
+namespace InMemoryLoaderTestComponent
+{
+	public class MyClass : AbstractPowerUpComponent
+	{
+		private static readonly ILog log = LogManager.GetLogger (typeof(MyClass));
+
+		public MyClass ()
+		{
+			log.DebugFormat ("Create a new instance of Type: {0}", this.GetType ().ToString ());
+		}
+
+		public int MultiplyTwoInt (int paramOne, int paramTwo)
+		{
+			return paramOne * paramTwo;
+		}
+
+		public int MultiplyThreeInt (int paramOne, int paramTwo, int paramThree)
+		{
+			return paramOne * paramTwo * paramThree;
+		}
+	}
+}
+
