92ac4605a64b3779058db7ddcc73c7f28942804d
Kay Stuckenschmidt <mailto.kaysta@gmail.com>
2016-01-13 21:23:48 +0100

    - Moved "ObjectHelper" to InMemoryLoader

---
+++ InMemoryLoader/Helper/ObjectHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace InMemoryLoader
+{
+	public class ObjectHelper
+	{
+		/// <summary>
+		/// Returns the Value
+		/// </summary>
+		/// <returns>The field value.</returns>
+		/// <param name="filedName">Filed name.</param>
+		/// <param name="paramObject">Parameter object.</param>
+		public static object GetFieldValue (string fieldName, object paramObject)
+		{
+			var value = paramObject.GetType ().GetField (fieldName).GetValue (paramObject);
+			return value;
+		}
+
+		/// <summary>
+		/// Gets the property value.
+		/// </summary>
+		/// <returns>The property value.</returns>
+		/// <param name="filedName">Filed name.</param>
+		/// <param name="paramObject">Parameter object.</param>
+		public static object GetPropertyValue (string propertyName, object paramObject)
+		{
+			var value = paramObject.GetType ().GetProperty (propertyName).GetValue (paramObject);
+			return value;
+		}
+	}
+}
+
