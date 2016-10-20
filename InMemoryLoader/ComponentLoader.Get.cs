0e94ac67c47f133698199ecfbab2473768be7b31
Kay Stuckenschmidt <kay.stuckenschmidt@maxomedia.ch>
2016-10-20 13:42:40 +0200

    code cleanup

--- InMemoryLoader/ComponentLoader.Get.cs
+++ InMemoryLoader/ComponentLoader.Get.cs
@@ -11,66 +11,79 @@ using InMemoryLoaderBase;
 
 namespace InMemoryLoader
 {
-	public partial class ComponentLoader
-	{
-		/// <summary>
-		/// Gets the class reference.
-		/// </summary>
-		/// <returns>The class reference.</returns>
-		/// <param name="AssemblyName">Assembly name.</param>
-		/// <param name="ClassName">Class name.</param>
-		public IDynamicClassInfo GetClassReference (string AssemblyName, string ClassName)
-		{
-			if (this.ClassReferences.ContainsKey (AssemblyName) == false) {
+    public partial class ComponentLoader
+    {
+        /// <summary>
+        /// Gets the class reference.
+        /// </summary>
+        /// <returns>The class reference.</returns>
+        /// <param name="AssemblyName">Assembly name.</param>
+        /// <param name="ClassName">Class name.</param>
+        public IDynamicClassInfo GetClassReference(string AssemblyName, string ClassName)
+        {
+            if (this.ClassReferences.ContainsKey(AssemblyName) == false)
+            {
 
-				Assembly assembly;
+                Assembly assembly;
 
-				if (this.AssemblyReferences.ContainsKey (AssemblyName) == false) {
-					this.AssemblyReferences.Add (AssemblyName, assembly = Assembly.LoadFrom (AssemblyName));
-				} else {
-					assembly = (Assembly)this.AssemblyReferences [AssemblyName];
-				}
+                if (this.AssemblyReferences.ContainsKey(AssemblyName) == false)
+                {
+                    this.AssemblyReferences.Add(AssemblyName, assembly = Assembly.LoadFrom(AssemblyName));
+                }
+                else
+                {
+                    assembly = (Assembly)this.AssemblyReferences[AssemblyName];
+                }
 
-				foreach (Type type in assembly.GetTypes()) {
+                foreach (Type type in assembly.GetTypes())
+                {
 
-					if (type.IsClass == true) {
+                    if (type.IsClass == true)
+                    {
 
-						if (type.IsSubclassOf (typeof(AbstractPowerUpComponent))) {
+                        if (type.IsSubclassOf(typeof(AbstractPowerUpComponent)))
+                        {
 
-							if (type.FullName.EndsWith ("." + ClassName)) {
+                            if (type.FullName.EndsWith("." + ClassName))
+                            {
 
-								IDynamicClassInfo classInfo = new DynamicClassInfo (type, Activator.CreateInstance (type));
-								this.ClassReferences.Add (AssemblyName, classInfo);
-								return (classInfo);
-							}
-						} else {
-							throw (new System.Exception ("Class is not typeof(AbstractPowerUpComponent)"));
-						}
-					}
+                                IDynamicClassInfo classInfo = new DynamicClassInfo(type, Activator.CreateInstance(type));
+                                this.ClassReferences.Add(AssemblyName, classInfo);
+                                return (classInfo);
+                            }
+                        }
+                        else
+                        {
+                            throw (new System.Exception("Class is not typeof(AbstractPowerUpComponent)"));
+                        }
+                    }
 
-				}
+                }
 
-				throw (new System.Exception ("Could not instantiate Class"));
-			}
+                throw (new System.Exception("Could not instantiate Class"));
+            }
 
-			return ((DynamicClassInfo)this.ClassReferences [AssemblyName]);
-		}
+            return ((DynamicClassInfo)this.ClassReferences[AssemblyName]);
+        }
 
-		/// <summary>
-		/// Gets the class reference.
-		/// </summary>
-		/// <returns>The class reference.</returns>
-		/// <param name="paramClassName">Parameter class name.</param>
-		public IDynamicClassInfo GetClassReference (string paramClassName)
-		{
-			var value = this.ClassReferences.Where (cls => cls.Value.ClassType.Name.Contains (paramClassName)).FirstOrDefault ();
+        /// <summary>
+        /// Gets the class reference.
+        /// </summary>
+        /// <returns>The class reference.</returns>
+        /// <param name="paramClassName">Parameter class name.</param>
+        public IDynamicClassInfo GetClassReference(string paramClassName)
+        {
+            var value = this.ClassReferences.Where(cls => cls.Value.ClassType.Name.Contains(paramClassName)).FirstOrDefault();
 
-			if (!string.IsNullOrEmpty (value.Key)) {
-				return value.Value;
-			} else {
-				return null;
-			}
-		}
-	}
+            if (!string.IsNullOrEmpty(value.Key))
+            {
+                return value.Value;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
 }
 
--- InMemoryLoader/ComponentLoader.Invoke.cs
+++ InMemoryLoader/ComponentLoader.Invoke.cs
@@ -11,43 +11,49 @@ using InMemoryLoaderBase;
 
 namespace InMemoryLoader
 {
-	public partial class ComponentLoader
-	{
-		/// <summary>
-		/// Invokes the method.
-		/// </summary>
-		/// <returns>The method.</returns>
-		/// <param name="ClassInfo">Class info.</param>
-		/// <param name="MethodName">Method name.</param>
-		/// <param name="paramArgs">Parameter arguments.</param>
-		public dynamic InvokeMethod (IDynamicClassInfo ClassInfo, string MethodName, Object[] paramArgs)
-		{
-			try {
-				var result = ClassInfo.ClassType.InvokeMember (MethodName, BindingFlags.Default | BindingFlags.InvokeMethod, null, ClassInfo.ClassObject, paramArgs);
-				return (result);
-			} catch (Exception ex) {
-				throw ex;
-			}
-		}
+    public partial class ComponentLoader
+    {
+        /// <summary>
+        /// Invokes the method.
+        /// </summary>
+        /// <returns>The method.</returns>
+        /// <param name="ClassInfo">Class info.</param>
+        /// <param name="MethodName">Method name.</param>
+        /// <param name="paramArgs">Parameter arguments.</param>
+        public dynamic InvokeMethod(IDynamicClassInfo ClassInfo, string MethodName, Object[] paramArgs)
+        {
+            try
+            {
+                var result = ClassInfo.ClassType.InvokeMember(MethodName, BindingFlags.Default | BindingFlags.InvokeMethod, null, ClassInfo.ClassObject, paramArgs);
+                return (result);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
 
-		/// <summary>
-		/// Invokes the method.
-		/// </summary>
-		/// <returns>The method.</returns>
-		/// <param name="AssemblyName">Assembly name.</param>
-		/// <param name="ClassName">Class name.</param>
-		/// <param name="MethodName">Method name.</param>
-		/// <param name="paramArgs">Parameter arguments.</param>
-		public dynamic InvokeMethod (string AssemblyName, string ClassName, string MethodName, Object[] paramArgs)
-		{
-			try {
-				var classInfo = this.GetClassReference (AssemblyName, ClassName);
-				var result = (this.InvokeMethod (classInfo, MethodName, paramArgs));
-				return result;
-			} catch (Exception ex) {
-				throw ex;
-			}
-		}
-	}
+        /// <summary>
+        /// Invokes the method.
+        /// </summary>
+        /// <returns>The method.</returns>
+        /// <param name="AssemblyName">Assembly name.</param>
+        /// <param name="ClassName">Class name.</param>
+        /// <param name="MethodName">Method name.</param>
+        /// <param name="paramArgs">Parameter arguments.</param>
+        public dynamic InvokeMethod(string AssemblyName, string ClassName, string MethodName, Object[] paramArgs)
+        {
+            try
+            {
+                var classInfo = this.GetClassReference(AssemblyName, ClassName);
+                var result = (this.InvokeMethod(classInfo, MethodName, paramArgs));
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
 }
 
